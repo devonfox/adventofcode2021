fe3caa5821560c531d507e4a22c3fff93cdfd804
devonfox <foxdev@pdx.edu>
2021-12-31 01:42:24 -0800

    05pt1 begin

---
+++ 05/05.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+struct point
+{
+
+};
+
+class graph
+{
+
+};
---
+++ 05/main.cpp
@@ -0,0 +1,7 @@
+#include "05.h"
+
+int main()
+{
+
+    return 0;
+}
