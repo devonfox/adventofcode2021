30474379a3e221cac4a151ea0b92f8b4d1ae56fb
devonfox <foxdev@pdx.edu>
2022-01-01 01:42:48 -0800

    06 update

--- 06/06.cpp
+++ 06/06.cpp
@@ -1,1 +1,26 @@
 #include "06.h"
+
+school::school()
+{
+    int input = 0;
+    ifstream in("input_06_sample.txt");
+
+    while(!in.eof())
+    {
+        in >> input;
+
+        lanternfish newfish(input);
+        seafloor.push_back(newfish);
+    }
+}
+
+int school::run()
+{
+
+}
+
+void school::print()
+{
+
+}
+
--- 06/06.h
+++ 06/06.h
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+struct lanternfish
+{
+    int timer;
+    lanternfish(int time)
+    {
+        timer = time;
+    }
+};
+
+class school
+{
+    public:
+        school();
+        int run();
+        void print();
+    
+    private:
+        vector<lanternfish> seafloor;
+}; 
