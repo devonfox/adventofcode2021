11a2a7caa28f229fabd8ff654657ecdd9078390f
devonfox <foxdev@pdx.edu>
2021-12-31 16:50:24 -0800

    05pt1 complete

--- 05/main.cpp
+++ 05/main.cpp
@@ -4,6 +4,10 @@ int main()
 {
     graph object;
     object.lineitup();
+    //object.print();  // just to test example output
+    int count = object.countitup();
+
+    cout << "Intersecting points: " << count << endl;
 
     return 0;
 }
